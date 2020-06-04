90e1227e47b6a86f870b95eb4b17b82ff86810d2
Mohammed-Alsaady <mohammedeydan@gmail.com>
2020-06-04 21:52:12 +0300

    commit react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,111 @@
-import React from 'react';
-import logo from './logo.svg';
+import React , {Component} from 'react'
 import './App.css';
 
-function App() {
+const list =[
+  {
+    title: 'React',
+    owner : 'Facebook',
+    url : 'www.reactjs.org',
+    Id : 0
+  },
+  {
+    title : 'Angular2',
+    owner: 'Google',
+    url: 'www.angular.io',
+    Id: 1
+  },
+  {
+    title: 'Vuejs',
+    owner: 'Evan',
+    url: 'www.vuejs.org',
+    Id : 2
+  }
+]
+function isSearched(val){
+  return function (item){
+    return (item.title.toLowerCase().includes(val.toLowerCase()));
+  }
+}
+class App extends Component {
+  constructor (props){
+    super(props) ;
+    this.state = {
+      list , //list ,  i = i+1 , i +=1
+      searchValue : '' 
+    }
+    //this.onClick =this.onClick.bind(this);
+    this.onChanged=this.onChanged.bind(this);
+    this.onDelete =this.onDelete.bind(this);
+  }
+ onDelete (i) {
+   const newList = this.state.list.filter(function (item){
+     return item.Id !== i ;
+   })
+   this.setState ({
+     list : newList 
+   })
+   
+ }
+ onClick() {
+   console.log(this)
+ }
+ onChanged (event){
+   this.setState({
+     searchValue: event.target.value
+   })
+
+ }
+  render() {
+    const {searchValue ,list} =this.state ;
+    return (
+      <div className='App'>
+        <Search 
+        value = {searchValue}
+        onChange = {this.onChanged}
+          >
+            <h3>Search with :</h3>
+            </Search>
+        <hr />
+        <Table 
+        list = {list}
+        Pattern = {searchValue}
+        onDelete ={this.onDelete}
+        />
+      </div>
+    )
+  }
+}
+class Search extends Component {
+render (){
+  const {value , onChange ,children} =this.props ;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <form>
+     {children} <input
+      type ="text"
+      value ={value}
+      onChange={onChange} >
+      </input>
+    </form>
+  )
+}
 }
 
-export default App;
+class Table extends Component{
+render () {
+  const {list , Pattern ,onDelete} = this.props ;
+  return (
+    <div>
+      {list.filter(isSearched(Pattern)).map (item =>
+      <div key={item.Id} >
+        <span>{item.title}</span>
+        &nbsp; By : <span>
+          <a href={item.url} >{item.owner}</a>
+        </span>
+
+      </div>
+        )}
+    </div>
+  )
+}
+}
+export default App ;
